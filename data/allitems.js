35da11af7f8c9425d01ec189497a3f9e808a74cb
Etignis <etignis@gmail.com>
2018-07-03 23:46:55 +0300

    fix[data]: potion name

--- data/allitems.js
+++ data/allitems.js
@@ -3073,7 +3073,7 @@ var allItems = [
   },
   {
     "en": {
-      "name": "POTION OF fire STRENGTH",
+      "name": "POTION OF FIRE GIANT STRENGTH",
       "type": "Potion",
       "rarity": 3,
       "text": "When you drink this potion, your Strength score became 25 for 1 hour. The potion has no effect on you if your Strength is equal to or greater than that score. <br>This potion's transparent liquid has floating in it a sliver of fingernail from a giant of the appropriate type. ",
